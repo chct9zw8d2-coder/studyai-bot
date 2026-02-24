2bcc8de525d8155bdf1a09a285a3cdf971855dee
chct9zw8d2-coder <chct9zw8d2@privaterelay.appleid.com>
2026-02-24 20:34:19 +0300

    Delete monetization/profit_guard.py

--- monetization/profit_guard.py
+++
@@ -1,9 +0,0 @@
-
-def should_reduce_limits(api_cost_today: float, revenue_today: float) -> bool:
-    """
-    If costs approach revenue, tighten FREE limits.
-    """
-    if revenue_today == 0:
-        return True
-    ratio = api_cost_today / revenue_today
-    return ratio > 0.6
